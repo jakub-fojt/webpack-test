9af3b27e39356a3b8a0e08e145bb584d43a4fe9e
Jakub Fojt <jakub.fojt.am@gmail.com>
2020-06-21 22:51:43 +0200

    new

--- webpack.common.js
+++ webpack.common.js
@@ -88,5 +88,11 @@ module.exports = {
       chunks: ["index", "another"],
       filename: "another.html",
     }),
+     new HtmlWebpackPlugin({
+      template: "./src/pages/kuba.html",
+      inject: true,
+      chunks: ["index", "another"],
+      filename: "kuba.html",
+    }),
   ],
 };
